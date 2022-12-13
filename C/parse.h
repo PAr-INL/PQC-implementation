a8a5a40b839eadd8a3bc64e9ac1909d050851f1f
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2022-12-13 15:02:02 +0100

    Function parse

---
+++ C/parse.h
@@ -0,0 +1,23 @@
+#include "tools.h"
+#include <math.h> 
+
+typedef Rq parse(int tab)
+{
+    int i = 0;
+    int j = 0;
+    while j < n{
+        d1 = bi + 256*(bi+1 mod+16) d2 = floor(bi+1/16)+16*bi+2
+        if d1 < q
+        {
+        a_chapeau_j = d1;
+        j++;
+        };
+        if d2 < q and j < n
+        {
+            a_chapeau_j = d2;
+            j++;
+        };
+        i = i + 3;
+    }
+        //return aˆ0 + aˆ1X + · · · + aˆn−1Xn−1
+}
